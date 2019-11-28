d6156ef499e3ab0d041c083a8ad748c1f32194c1
Anna Suntsova <anna_suntsova@epam.com>
2019-11-28 15:38:14 +0400

    111

---
+++ Module 5/FileWatcher/FileWatcher/FileWatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using FileWatcher.Resources;
+
+namespace FileWatcher
+{
+    class FileWatcher
+    {
+        static void Main(string[] args)
+        {
+            RunWatching();
+        }
+
+        private static void RunWatching()
+        {
+            var systemWatcher=new FileSystemWatcher();
+            systemWatcher.Path = @"D:\Info";
+            systemWatcher.NotifyFilter = NotifyFilters.LastAccess;
+            systemWatcher.Filter = "*.*";
+            systemWatcher.Created += SystemWatcher_Created;
+            systemWatcher.EnableRaisingEvents = true;
+        }
+
+        private static void SystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            //var rm= new ResourceManager ()
+            //Console.WriteLine(ResourceEn.FileCreated);
+            var newPath = Path.Combine(Path.GetTempPath(), Path.GetFileName(e.Name));
+
+            //поиск правила
+            if (false) Console.Write(ResourceEn.RuleNotFound);
+            else
+            {
+                Console.WriteLine(ResourceEn.RuleFound);
+                File.Move(e.Name, newPath);
+                Console.WriteLine(ResourceEn.FileTransferToDestination);
+            }
+        }
+    }
+}
